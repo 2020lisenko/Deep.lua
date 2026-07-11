a5a9f042c80ef10c1fa372b5d10042d057d5f49c
2020lisenko <lysenkoila439@gmail.com>
2026-07-11 19:24:56 +0500

    Remove ModuleLoader implementation

--- ModuleLoader.lua
+++ ModuleLoader.lua
@@ -1,9 +1,1 @@
-local ModuleLoader = {}
-ModuleLoader.__index = ModuleLoader
 
-function ModuleLoader:LoadModule(moduleName, ...)
-	local module = loadstring(game:HttpGet(self.Repo .. "modules/" .. moduleName .. ".lua"))()
-	return module:Initialize(...)
-end
-
-return setmetatable({Repo = game:HttpGet("")}, ModuleLoader)
