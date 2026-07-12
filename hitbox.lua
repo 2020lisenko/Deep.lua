44f6e2cb90d27cd5511f5e0caaa8ac8177f3d6ef
2020lisenko <lysenkoila439@gmail.com>
2026-07-12 17:24:59 +0500

    Update hitbox.lua

--- hitbox.lua
+++ hitbox.lua
@@ -10,6 +10,14 @@ function Hitbox:Initialize(Tab)
     
     self.Enabled = false
     self.Size = 1
+    self.SelectedParts = {
+        Head = true,
+        Torso = true,
+        LeftArm = false,
+        RightArm = false,
+        LeftLeg = false,
+        RightLeg = false
+    }
     self.Connections = {}
     self.OriginalSizes = {}
     
@@ -39,8 +47,96 @@ function Hitbox:Initialize(Tab)
         end
     })
     
+    HitboxGroup:AddDivider()
+    HitboxGroup:AddLabel("Body Parts")
+    
+    HitboxGroup:AddToggle("HeadPart", {
+        Text = "Head",
+        Default = true,
+        Callback = function(v)
+            self.SelectedParts.Head = v
+        end
+    })
+    
+    HitboxGroup:AddToggle("TorsoPart", {
+        Text = "Torso",
+        Default = true,
+        Callback = function(v)
+            self.SelectedParts.Torso = v
+        end
+    })
+    
+    HitboxGroup:AddToggle("LeftArmPart", {
+        Text = "Left Arm",
+        Default = false,
+        Callback = function(v)
+            self.SelectedParts.LeftArm = v
+        end
+    })
+    
+    HitboxGroup:AddToggle("RightArmPart", {
+        Text = "Right Arm",
+        Default = false,
+        Callback = function(v)
+            self.SelectedParts.RightArm = v
+        end
+    })
+    
+    HitboxGroup:AddToggle("LeftLegPart", {
+        Text = "Left Leg",
+        Default = false,
+        Callback = function(v)
+            self.SelectedParts.LeftLeg = v
+        end
+    })
+    
+    HitboxGroup:AddToggle("RightLegPart", {
+        Text = "Right Leg",
+        Default = false,
+        Callback = function(v)
+            self.SelectedParts.RightLeg = v
+        end
+    })
+    
     print("Hitbox module loaded!")
     return self
+end
+
+function Hitbox:ShouldModifyPart(partName)
+    local name = partName:lower()
+    
+    if name == "head" and self.SelectedParts.Head then
+        return true
+    elseif (name:find("torso") or name:find("upper") or name:find("lower")) and self.SelectedParts.Torso then
+        return true
+    elseif (name:find("left") and name:find("arm")) and self.SelectedParts.LeftArm then
+        return true
+    elseif (name:find("right") and name:find("arm")) and self.SelectedParts.RightArm then
+        return true
+    elseif (name:find("left") and name:find("leg")) and self.SelectedParts.LeftLeg then
+        return true
+    elseif (name:find("right") and name:find("leg")) and self.SelectedParts.RightLeg then
+        return true
+    end
+    
+    return false
+end
+
+function Hitbox:GetNewSize(partName)
+    local name = partName:lower()
+    local size = self.Size
+    
+    if name == "head" then
+        return Vector3.new(size * 2, size * 1.5, size * 1.5)
+    elseif name:find("torso") or name:find("upper") or name:find("lower") then
+        return Vector3.new(size * 2, size * 2, size * 1.5)
+    elseif name:find("arm") then
+        return Vector3.new(size, size * 2, size)
+    elseif name:find("leg") then
+        return Vector3.new(size, size * 2, size)
+    end
+    
+    return nil
 end
 
 function Hitbox:Start()
@@ -52,23 +148,26 @@ function Hitbox:Start()
             if player ~= Players.LocalPlayer and player.Character then
                 for _, part in pairs(player.Character:GetChildren()) do
                     if part:IsA("BasePart") and part.Name ~= "HumanoidRootPart" then
-                        -- Сохраняем оригинальный размер
-                        if not self.OriginalSizes[part] then
-                            self.OriginalSizes[part] = part.Size
+                        if self:ShouldModifyPart(part.Name) then
+                            -- Сохраняем оригинальный размер
+                            if not self.OriginalSizes[part] then
+                                self.OriginalSizes[part] = part.Size
+                            end
+                            
+                            -- Увеличиваем размер
+                            local newSize = self:GetNewSize(part.Name)
+                            if newSize then
+                                part.Size = newSize
+                                part.Transparency = 0.7
+                            end
+                        else
+                            -- Восстанавливаем размер если часть не выбрана
+                            if self.OriginalSizes[part] then
+                                part.Size = self.OriginalSizes[part]
+                                part.Transparency = 0
+                                self.OriginalSizes[part] = nil
+                            end
                         end
-                        
-                        -- Увеличиваем размер в зависимости от части тела
-                        local newSize = part.Size
-                        if part.Name == "Head" then
-                            newSize = Vector3.new(self.Size * 2, self.Size * 1.5, self.Size * 1.5)
-                        elseif part.Name:find("Torso") then
-                            newSize = Vector3.new(self.Size * 2, self.Size * 2, self.Size * 1.5)
-                        elseif part.Name:find("Arm") or part.Name:find("Leg") then
-                            newSize = Vector3.new(self.Size, self.Size * 2, self.Size)
-                        end
-                        
-                        part.Size = newSize
-                        part.Transparency = 0.7
                     end
                 end
             end
